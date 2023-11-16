cb239c109df1a37f3d48028f51f874826fa25804
whbcmon <1652725215@qq.com>
2023-11-16 14:46:07 +0800

    Site updated: 2023-11-16 14:46:06

---
+++ self-js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "12043",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "000000",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 8px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "left",
+    "key": "788262f16df6476eb46b946b052faac7"
+  }
+}
