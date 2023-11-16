23c5effd3b8cac1597d1abc0dd980acaea175cec
whbcmon <1652725215@qq.com>
2023-11-16 14:56:41 +0800

    Site updated: 2023-11-16 14:56:40

--- self-js/weather.js
+++ self-js/weather.js
@@ -1,6 +1,6 @@
 WIDGET = {
   "CONFIG": {
-    "modules": "12043",
+    "modules": "12043", // 0：温度 1：城市 2：图像 3：预警 4：空气质量。分顺序
     "background": "5",
     "tmpColor": "000000",
     "tmpSize": "16",
@@ -10,13 +10,13 @@ WIDGET = {
     "aqiSize": "16",
     "weatherIconSize": "24",
     "alertIconSize": "18",
-    "padding": "10px 10px 8px 10px",
+    "padding": "10px 10px 8px 10px", // 上右下左
     "shadow": "0",
     "language": "auto",
     "borderRadius": "5",
     "fixed": "false",
     "vertical": "center",
-    "horizontal": "left",
+    "horizontal": "left", // 左对齐
     "key": "788262f16df6476eb46b946b052faac7"
   }
 }
