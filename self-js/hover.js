8ca002d91b13d62e6f6f3e477454f60fdf38f6e8
whbcmon <1652725215@qq.com>
2023-11-20 23:26:30 +0800

    Site updated: 2023-11-20 23:26:29

---
+++ self-js/hover.js
@@ -0,0 +1,29 @@
+// // 鼠标移入事件
+// document.querySelector('.test').onmouseover = function() {
+//   document.querySelector('.test').style.color = 'blue'
+// }
+
+// // 鼠标移出事件
+// document.querySelector('.test').onmouseout  = function() {
+//   document.querySelector('.test').style.color = 'unset'
+// }
+// document.querySelector("#menus > div.menus_items > div:nth-child(4)")
+
+
+// 获取 DOM 元素列表
+var elements = document.querySelectorAll("#menus > div.menus_items > *, #search-button");
+
+// 为每个元素添加事件监听器
+elements.forEach(function(element) {
+  // 鼠标移入事件
+  element.addEventListener('mouseover', function() {
+    // element.children[0].children[1].style.color = 'red';
+    element.children[0].children[1].style.fontSize = '20px';
+  });
+
+  // 鼠标移出事件
+  element.addEventListener('mouseout', function() {
+    // element.children[0].children[1].style.color = 'unset';
+    element.children[0].children[1].style.fontSize = 'unset';
+  });
+});
