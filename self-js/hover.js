ccbfc9d5dba5a507ecf8a75f67ac4d30d2cc6dd8
whbcmon <1652725215@qq.com>
2023-11-21 17:26:25 +0800

    Site updated: 2023-11-21 17:26:23

--- self-js/hover.js
+++ self-js/hover.js
@@ -11,7 +11,7 @@
 
 
 // 获取 DOM 元素列表
-var elements = document.querySelectorAll("#menus > div.menus_items > *, #search-button");
+var elements = document.querySelectorAll("#menus > div.menus_items > *, #search-button, #nav .menus_items .menus_item .menus_item_child li");
 
 // 为每个元素添加事件监听器
 elements.forEach(function(element) {
@@ -25,7 +25,7 @@ elements.forEach(function(element) {
   // 鼠标移出事件
   element.addEventListener('mouseout', function() {
     // element.children[0].children[1].style.color = 'unset';
-    element.children[0].children[0].style.fontSize = '20px'; // iconfont
+    element.children[0].children[0].style.fontSize = 'inherit'; // iconfont
     element.children[0].children[1].style.fontSize = 'inherit'; // text
   });
 });
