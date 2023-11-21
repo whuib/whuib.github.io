0aa5299afd8c4f6c65fda5d2f7a690187746a85a
whbcmon <1652725215@qq.com>
2023-11-21 23:25:02 +0800

    Site updated: 2023-11-21 23:25:01

--- self-js/weather.js
+++ self-js/weather.js
@@ -25,8 +25,12 @@ function insertAfter() {
   var weather = document.createElement('div');
   weather.id = 'he-plugin-simple'; // weather元素，用来显示天气
 
-  var nullElement = document.createElement('div');
-  nullElement.id = 'null_element'; // 空元素，用来扩展位置添加flex属性，并且在weather.css中剥夺掉blog-name的flex属性
+  var null_mid = document.createElement('div');
+  null_mid.id = 'null_mid'; // 空元素，用来扩展位置
+  var null_left = document.createElement('div');
+  null_left.id = 'null_left'; // 空元素，占位
+  var null_right = document.createElement('div');
+  null_right.id = 'null_right'; // 空元素，占位
 
   var search = document.createElement('div');
   search.id = 'search'; // search元素，用来装搜索button
@@ -35,11 +39,11 @@ function insertAfter() {
   var parent = currentElement.parentNode;
 
   parent.insertBefore(search, currentElement.nextSibling);
-  parent.insertBefore(nullElement, currentElement.nextSibling);
+  parent.insertBefore(null_mid, currentElement.nextSibling);
   parent.insertBefore(weather, currentElement.nextSibling);
   search.appendChild(document.getElementById('search-button'));
-  
+
+  parent.insertBefore(null_left, parent.firstChild);
+  parent.appendChild(null_right);
 }
 insertAfter();
-
-
